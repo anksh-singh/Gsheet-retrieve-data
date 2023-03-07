693c4bde24989dadf97f2351853ed756b80da508
ankitsingh1999 <ankitsi031999@gmail.com>
2023-03-07 16:47:03 +0530

    Group routing urls

--- web/routes.go
+++ web/routes.go
@@ -1,23 +1,23 @@
-// package web
+package web
 
-// import (
-// 	"sheet-retrieve/config"
-// 	grpcClient "sheet-retrieve/pkg/grpc/client"
-// 	"sheet-retrieve/web/handler"
+import (
+	"sheet-retrieve/config"
+	grpcClient "sheet-retrieve/pkg/grpc/client"
+	"sheet-retrieve/web/handler"
 
-// 	"github.com/gin-gonic/gin"
-// 	swaggerFiles "github.com/swaggo/files"     // swagger embed files
-// 	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
-// 	"go.uber.org/zap"
-// )
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"     // swagger embed files
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
+	"go.uber.org/zap"
+)
 
-// func InitRoutes(config *config.Config, logger *zap.SugaredLogger, route *gin.Engine) {
-// 	grpcClientManager := grpcClient.NewGrpcClientManager(config, logger)
-// 	webHandler := handler.NewHandler(config, logger, grpcClientManager)
+func InitRoutes(config *config.Config, logger *zap.SugaredLogger, route *gin.Engine) {
+	grpcClientManager := grpcClient.NewGrpcClientManager(config, logger)
+	webHandler := handler.NewHandler(config, logger, grpcClientManager)
 
-// 	groupRoute := route.Group("/api")
-// 	groupRoute.GET("/retrieve-sheet", webHandler.RetrieveSheet)
+	groupRoute := route.Group("/api")
+	groupRoute.GET("/retrieve-sheet", webHandler.RetrieveSheet)
 
-// 	//swagger api
-// 	route.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-// }
+	//swagger api
+	route.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+}
